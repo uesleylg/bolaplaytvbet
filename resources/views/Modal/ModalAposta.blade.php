0653dbc3aaa574b74ba45daa6956989b0f25b6d8
Uesley <uesleylaurianogomes@gmail.com>
2025-10-29 21:36:23 -0300

    faltando o rank mobile

--- resources/views/Modal/ModalAposta.blade.php
+++ resources/views/Modal/ModalAposta.blade.php
@@ -1,8 +1,4 @@
-<style>
-  .odds {
-  gap: 0.5rem; /* Espaço entre os botões */
-}
-</style>
+
  <div class="modal fade" id="ModalAposta" tabindex="-1" aria-labelledby="ModalAposta" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered"><!-- centraliza verticalmente -->
       <div class="modal-content" style="border-radius: 10px;">
@@ -14,22 +10,22 @@
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Fechar"></button>
       </div>
         
-        <div class="modal-body text-center" style="padding-inline: 0px; max-height: 70vh; overflow-y: auto;">
+        <div class="modal-body text-center" style="padding-inline: 0px; max-height: 90vh; overflow-y: auto;">
           
         
 
 
-            <div class="container" >
+            <div class="container container-mobile" >
 
       <!-- Botão limpar -->
-        <div class="text-end mb-3">
-          <button class="btn btn-warning rounded-pill px-4 py-2 fw-semibold">
-            <i class="fa-solid fa-broom me-2"></i> Limpar apostas
+        <div class="text-end mb-3 btt-limpar">
+          <button class="btn btn-warning rounded-pill px-4 py-2 fw-semibold btt-font-limp">
+            <i class="fa-solid fa-broom me-2 "></i> Limpar apostas
           </button>
         </div>
 
         <!-- Cabeçalho colunas -->
-        <div class="p-3 mb-2" style="background-color: #0f172a; border-radius: 10px;">
+        <div class="p-3 mb-2 cabe-aposta" >
           <div class="d-flex justify-content-between text-center text-uppercase fw-semibold small text-light">
  
             <div style="width: 45%;">Mandante</div>
@@ -41,208 +37,270 @@
    
     <!-- Jogo 1 -->
 
-<div class="game-card p-2">
-  <div class="d-flex align-items-center mb-2">
+<div class="game-card ">
+  <div class="d-flex align-items-center ">
     <div class="match-number">1</div>
     <div class="league ms-2">
       🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões 
     </div>
   </div>
 
-  <div class="d-flex justify-content-between align-items-stretch odds">
+  <div class="d-flex justify-content-between align-items-center odds">
     <!-- Time da esquerda -->
-    <button class="team-btn d-flex flex-column align-items-center justify-content-center">
-      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo mb-1">
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo me-2">
       <span>ARSENAL-ENG</span>
     </button>
 
     <!-- Empate -->
-    <button class="draw-btn d-flex align-items-center justify-content-center">
+    <button class="draw-btn d-flex align-items-center justify-content-center btt-empate">
       EMPATE
     </button>
 
     <!-- Time da direita -->
-    <button class="team-btn d-flex flex-column align-items-center justify-content-center">
-      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo mb-1">
+    <button class="team-btn d-flex align-items-center justify-content-center">
       <span>MADRID-ESP</span>
+      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo ms-2">
+      
     </button>
   </div>
+
+  
 </div>
 
-    <!-- Jogo 2 -->
-   <div class="game-card p-2">
-  <div class="d-flex align-items-center mb-2">
+
+
+    <!-- Jogo 1 -->
+
+<div class="game-card ">
+  <div class="d-flex align-items-center ">
     <div class="match-number">1</div>
     <div class="league ms-2">
       🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões 
     </div>
   </div>
 
-  <div class="d-flex justify-content-between align-items-stretch odds">
+  <div class="d-flex justify-content-between align-items-center odds">
     <!-- Time da esquerda -->
-    <button class="team-btn d-flex flex-column align-items-center justify-content-center">
-      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo mb-1">
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo me-2">
       <span>ARSENAL-ENG</span>
     </button>
 
     <!-- Empate -->
-    <button class="draw-btn d-flex align-items-center justify-content-center">
+    <button class="draw-btn d-flex align-items-center justify-content-center btt-empate">
       EMPATE
     </button>
 
     <!-- Time da direita -->
-    <button class="team-btn d-flex flex-column align-items-center justify-content-center">
-      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo mb-1">
+    <button class="team-btn d-flex align-items-center justify-content-center">
       <span>MADRID-ESP</span>
+      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo ms-2">
+      
     </button>
   </div>
+
+  
 </div>
 
 
-     <!-- Jogo 2 -->
-    <div class="game-card">
-      <div class="d-flex align-items-center mb-2">
-        <div class="match-number">3</div>
-        <div class="league">
-          🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões - Fase da Liga
-        </div>
-      </div>
-      <div class="row text-center g-2 odds">
-        <div class="col-4">
-          <button>LEVERKUSEN-ALE <small></small></button>
-        </div>
-        <div style="width:20%;" class="col-4">
-          <button>EMPATE <small></small></button>
-        </div>
-        <div class="col-4">
-          <button>PSG-FRA <small></small></button>
-        </div>
-      </div>
+
+
+    <!-- Jogo 1 -->
+
+<div class="game-card ">
+  <div class="d-flex align-items-center ">
+    <div class="match-number">1</div>
+    <div class="league ms-2">
+      🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões 
     </div>
+  </div>
 
+  <div class="d-flex justify-content-between align-items-center odds">
+    <!-- Time da esquerda -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo me-2">
+      <span>ARSENAL-ENG</span>
+    </button>
 
-     <!-- Jogo 2 -->
-    <div class="game-card">
-      <div class="d-flex align-items-center mb-2">
-        <div class="match-number">4</div>
-        <div class="league">
-          🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões - Fase da Liga
-        </div>
-      </div>
-      <div class="row text-center g-2 odds">
-        <div class="col-4">
-          <button>LEVERKUSEN-ALE <small></small></button>
-        </div>
-        <div style="width:20%;" class="col-4">
-          <button>EMPATE <small></small></button>
-        </div>
-        <div class="col-4">
-          <button>PSG-FRA <small></small></button>
-        </div>
-      </div>
-    </div>
+    <!-- Empate -->
+    <button class="draw-btn d-flex align-items-center justify-content-center btt-empate">
+      EMPATE
+    </button>
 
+    <!-- Time da direita -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <span>MADRID-ESP</span>
+      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo ms-2">
+      
+    </button>
+  </div>
 
-     <!-- Jogo 2 -->
-    <div class="game-card">
-      <div class="d-flex align-items-center mb-2">
-        <div class="match-number">5</div>
-        <div class="league">
-          🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões - Fase da Liga
-        </div>
-      </div>
-      <div class="row text-center g-2 odds">
-        <div class="col-4">
-          <button>LEVERKUSEN-ALE <small></small></button>
-        </div>
-        <div style="width:20%;" class="col-4">
-          <button>EMPATE <small></small></button>
-        </div>
-        <div class="col-4">
-          <button>PSG-FRA <small></small></button>
-        </div>
-      </div>
-    </div>
-
-
-     <!-- Jogo 2 -->
-    <div class="game-card">
-      <div class="d-flex align-items-center mb-2">
-        <div class="match-number">6</div>
-        <div class="league">
-          🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões - Fase da Liga
-        </div>
-      </div>
-      <div class="row text-center g-2 odds">
-        <div class="col-4">
-          <button>LEVERKUSEN-ALE <small></small></button>
-        </div>
-        <div style="width:20%;" class="col-4">
-          <button>EMPATE <small></small></button>
-        </div>
-        <div class="col-4">
-          <button>PSG-FRA <small></small></button>
-        </div>
-      </div>
-    </div>
-
-    <!-- Jogo 3 -->
-    <div class="game-card">
-      <div class="d-flex align-items-center mb-2">
-        <div class="match-number">7</div>
-        <div class="league">
-          🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões - Fase da Liga
-        </div>
-      </div>
-      <div class="row text-center g-2 odds">
-        <div class="col-4">
-          <button>NEWCASTLE-ENG <small></small></button>
-        </div>
-        <div style="width:20%;" class="col-4">
-          <button>EMPATE <small></small></button>
-        </div>
-        <div class="col-4">
-          <button>BENFICA-POR <small></small></button>
-        </div>
-      </div>
-    </div>
-<div style="text-align: left;">
-  <i class="fa-solid fa-circle-info"></i> Você precisa apostar em <b> todos os 8 jogos obrigatoriamente da rodada.</b><br>
-
-<i class="fa-solid fa-circle-info"></i> Ainda esta com duvidas? <b>Clique aqui e saiba mais</b>
-
-
+  
 </div>
+
+
+
+
+
+    <!-- Jogo 1 -->
+
+<div class="game-card ">
+  <div class="d-flex align-items-center ">
+    <div class="match-number">1</div>
+    <div class="league ms-2">
+      🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões 
+    </div>
+  </div>
+
+  <div class="d-flex justify-content-between align-items-center odds">
+    <!-- Time da esquerda -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo me-2">
+      <span>ARSENAL-ENG</span>
+    </button>
+
+    <!-- Empate -->
+    <button class="draw-btn d-flex align-items-center justify-content-center btt-empate">
+      EMPATE
+    </button>
+
+    <!-- Time da direita -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <span>MADRID-ESP</span>
+      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo ms-2">
+      
+    </button>
+  </div>
+
+  
+</div>
+
+
+
+
+    <!-- Jogo 1 -->
+
+<div class="game-card ">
+  <div class="d-flex align-items-center ">
+    <div class="match-number">1</div>
+    <div class="league ms-2">
+      🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões 
+    </div>
+  </div>
+
+  <div class="d-flex justify-content-between align-items-center odds">
+    <!-- Time da esquerda -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo me-2">
+      <span>ARSENAL-ENG</span>
+    </button>
+
+    <!-- Empate -->
+    <button class="draw-btn d-flex align-items-center justify-content-center btt-empate">
+      EMPATE
+    </button>
+
+    <!-- Time da direita -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <span>MADRID-ESP</span>
+      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo ms-2">
+      
+    </button>
+  </div>
+
+  
+</div>
+
+
+
+
+
+<div class="game-card ">
+  <div class="d-flex align-items-center ">
+    <div class="match-number">1</div>
+    <div class="league ms-2">
+      🔍 21/10 16:00 — <strong>EUROPA:</strong> Liga dos Campeões 
+    </div>
+  </div>
+
+  <div class="d-flex justify-content-between align-items-center odds">
+    <!-- Time da esquerda -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <img src="https://static.flashscore.com/res/image/data/xCI9eFAN-OS9FbeGg.png" class="team-logo me-2">
+      <span>ARSENAL-ENG</span>
+    </button>
+
+    <!-- Empate -->
+    <button class="draw-btn d-flex align-items-center justify-content-center btt-empate">
+      EMPATE
+    </button>
+
+    <!-- Time da direita -->
+    <button class="team-btn d-flex align-items-center justify-content-center">
+      <span>MADRID-ESP</span>
+      <img src="https://static.flashscore.com/res/image/data/Kv2QSkhT-nLBjwR1F.png" class="team-logo ms-2">
+      
+    </button>
+  </div>
+
+  
+</div>
+  
+
   </div>
 
 
 
-        </div>
+      
 
 
-           <div class="modal-footer">
+        
             
-             <div class="container text-center mt-4">
-  <div class="row justify-content-center g-3">
-    <!-- Valor da aposta -->
-    <div style="width: 50%;" class="col-12 col-md-4">
-      <div class="p-3 bg-dark text-light rounded-4 shadow-sm">
-       
-        <div class="fw-bold mt-2">VALOR DA APOSTA</div>
-        <div class="fs-4">R$ 10,00</div>
+          <div class="container container-mobile ">
+  <div class=" bg-dark text-white border-0 shadow-lg rounded-4 p-4" style="    margin-bottom: 15px; background: linear-gradient(145deg, #0f172a, #1e293b);">
+    <!-- Cabeçalho -->
+    <div class="d-flex justify-content-between align-items-center mb-3">
+      <h5 class="fw-bold mb-0 text-warning text-warning-mobile">
+        <i class="bi bi-ticket-perforated me-2"></i>
+        Aposta equivalente à <span class="text-white">1 bilhete(s)</span>
+      </h5>
+    
+    </div>
+
+    <!-- Tipos de apostas -->
+    <div class="row text-center mb-3">
+      <div class="col">
+        <div class="fw-bold text-uppercase text-secondary small">Secos</div>
+        <div class="fs-5 fw-bold text-light">0</div>
+      </div>
+      <div class="col">
+        <div class="fw-bold text-uppercase text-secondary small">Duplos</div>
+        <div class="fs-5 fw-bold text-light">0</div>
+      </div>
+      <div class="col">
+        <div class="fw-bold text-uppercase text-secondary small">Triplos</div>
+        <div class="fs-5 fw-bold text-light">0</div>
       </div>
     </div>
 
-    <!-- Premiação estimada -->
-    <div style="width: 50%; " class="col-12 col-md-4">
-      <div class="p-3 bg-dark text-light rounded-4 shadow-sm">
-       
-        <div class="fw-bold mt-2">PREMIAÇÃO ESTIMADA</div>
-        <div class="fs-4">R$ 2.000,00</div>
-      </div>
+    <hr class="border-secondary opacity-50 my-3">
+
+    <!-- Valor total -->
+    <div class="d-flex justify-content-between align-items-center mb-2">
+      <span class="fw-semibold text-secondary">Valor Apostado</span>
+      <h4 class="fw-bold text-success mb-0">R$ 10,00</h4>
     </div>
+
+    <!-- Retorno fixo -->
+    <div class="d-flex justify-content-between align-items-center">
+      <span class="fw-semibold text-secondary">Premio Estimado</span>
+      <h4 class="fw-bold text-warning mb-0 ">R$ 50.000,00</h4>
+    </div>
+
   </div>
-</div>
+
+  </div>
         
           <button  type="button" class="btn btn-primary btt-conf">CONFIRMAR APOSTA</button>
         </div>
